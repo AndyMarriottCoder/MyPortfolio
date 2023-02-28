1727b97a4572cd69d481c37198121e9ed3fac2ae
Andy Marriott <andymarriott886@googlemail.com>
2023-02-28 12:18:15 +0000

    Removed photo while Vercel investgations continue

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import { useTypewriter } from 'react-simple-typewriter';
-import myImage from '../images/PhotoOfMe.png';
+// import myImage from '../images/PhotoOfMe.png';
 // import myImage from 'next/image';
 
 type Props = {}
@@ -19,7 +19,7 @@ export default function Intro({}: Props) {
   // Styling of the font and the buttons
   return (
     <div className="bg-black text-yellow-300 h-screen flex flex-col items-center justify-center text-center overflow-hidden">
-    <img className="relative rounded-full h-40 w-50 mx-auto pb-4" src={myImage.src} />
+    {/* <img className="relative rounded-full h-40 w-50 mx-auto pb-4" src={myImage.src} /> */}
     <h2 className="text-lg uppercase text-yellow-300 pb-6 pt-6 tracking-[15px]">
     Software Development Engineer II
     </h2>
