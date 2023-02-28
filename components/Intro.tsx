1cfa19ccea81fdb7b7d2098e4c7ee5ac300a6fcf
Andy Marriott <andymarriott886@googlemail.com>
2023-02-28 15:39:16 +0000

    Added profile picture and modified text

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import Link from 'next/link';
 import { useTypewriter } from 'react-simple-typewriter';
-// import myImage from '../images/PhotoOfMe.png';
-// import myImage from 'next/image';
+import profilePic from '../images/PhotoOfMe.png';
+import Image from 'next/image';
 
 type Props = {}
 
 // Function displaying the TypeWriter functions
 export default function Intro({}: Props) {
   const [text] = useTypewriter({
-    words: ['Welcome To My Portfolio', 'I\'m Andy Marriott'],
+    words: ['Welcome To My Portfolio!'],
     loop: 5,
     typeSpeed: 100,
     delaySpeed: 1000,
-    onLoopDone: () => console.log('loop completed after 5 runs.')
+    onLoopDone: () => console.log('loop completed after 10 runs.')
   });
 
   // Styling of the font and the buttons
   return (
     <div className="bg-black text-yellow-300 h-screen flex flex-col items-center justify-center text-center overflow-hidden">
-    {/* <img className="relative rounded-full h-40 w-50 mx-auto pb-4" src={myImage.src} /> */}
-    <h2 className="text-lg uppercase text-yellow-300 pb-6 pt-6 tracking-[15px]">
+    <Image className="rounded-full pb-4" src={profilePic} alt="Profile Picture" width={600} height={600}/>
+    <h2 className="animate-bounce text-lg uppercase text-yellow-300 pb-6 pt-6 tracking-[15px]">
     Software Development Engineer II
-    </h2>
+    </h2> 
     <h1 className="lg:text-6xl px-10">
       <span className="inline-block">{text}</span>
     </h1>
--- pages/Header2.tsx
+++ pages/Header2.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import Link from 'next/link'
 
 // Function to link up mail image to email address
 function mailto(){
@@ -44,10 +45,10 @@ function Header2() {
                 <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-white rounded lg:bg-transparent lg:text-yellow-300 lg:p-0 dark:text-white" aria-current="page">Home</a>
+                            <Link href="/" className="block py-2 pr-4 pl-3 text-white rounded lg:bg-transparent lg:text-yellow-300 lg:p-0 dark:text-white" aria-current="page">Home</Link>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                            <Link href="/about" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</Link>
                         </li>
                         <li>
                             <a href="#" className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
